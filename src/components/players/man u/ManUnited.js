b62d31a88d1cce3541561f2028b0a847982811f3
JeffVeron <67024868+JeffVeron@users.noreply.github.com>
2022-06-20 20:19:48 +0000

    Add files via upload

---
+++ src/components/players/man u/ManUnited.js
@@ -0,0 +1,205 @@
+
+const manUnitedPlayerData = [
+    {
+        playerId : 1 ,
+        playerPosition : "Goalkeeper",
+        playerCountry : "England",
+        playerImage : require ("./heaton.png"),
+        playerName : "Tom Heaton"
+    },
+    {
+        playerId : 2 ,
+        playerPosition : "Goalkeeper",
+        playerCountry : "England",
+        playerImage : require ("./henderson.png"),
+        playerName : "Dean Henderson"
+    },
+
+    {
+        playerId : 3 ,
+        playerPosition : "Goalkeeper",
+        playerCountry : "England",
+        playerImage : require ("./grant.png"),
+        playerName : "Lee Grant"
+    },
+
+    {
+        playerId : 4 ,
+        playerPosition : "Goalkeeper",
+        playerCountry : "Spain",
+        playerImage : require ("./deGea.png"),
+        playerName : "David de Gea"
+    },
+    {
+        playerId : 5 ,
+        playerPosition : "Defender",
+        playerCountry : "England",
+        playerImage : require ("./jones.png"),
+        playerName : "Phil Jones"
+    },
+    {
+        playerId : 6 ,
+        playerPosition : "Defender",
+        playerCountry : "Sweden",
+        playerImage : require ("./lindelof.png"),
+        playerName : "Victor Nilsson Lindelof"
+    },
+    {
+        playerId : 7 ,
+        playerPosition : "Defender",
+        playerCountry : "Ivory Coast",
+        playerImage : require ("./bailly.png"),
+        playerName : "Eric Bailly"
+    },
+    {
+        playerId : 8 ,
+        playerPosition : "Defender",
+        playerCountry : "France",
+        playerImage : require ("./varane.png"),
+        playerName : "Raphael Varane"
+    },
+    {
+        playerId : 9 ,
+        playerPosition : "Defender",
+        playerCountry : "England",
+        playerImage : require ("./shaw.png"),
+        playerName : "Luke Shaw"
+    },
+    {
+        playerId : 10 ,
+        playerPosition : "Defender",
+        playerCountry : "Portugal",
+        playerImage : require ("./dalot.png"),
+        playerName : "Diogo Dalot"
+    },
+    {
+        playerId : 11 ,
+        playerPosition : "Defender",
+        playerCountry : "England",
+        playerImage : require ("./maguire.png"),
+        playerName : "Harry Maguire"
+    },
+    {
+        playerId : 12 ,
+        playerPosition : "Defender",
+        playerCountry : "Brazil",
+        playerImage : require ("./telles.png"),
+        playerName : "Alex Telles"
+    },
+    {
+        playerId : 13 ,
+        playerPosition : "Defender",
+        playerCountry : "England",
+        playerImage : require ("./wan-bissaka.png"),
+        playerName : "Aaron Wan-Bissaka"
+    },
+    {
+        playerId : 14 ,
+        playerPosition : "Midfielder",
+        playerCountry : "Spain",
+        playerImage : require ("./mata.png"),
+        playerName : "Juan Mata"
+    },
+    {
+        playerId : 15,
+        playerPosition : "Midfielder",
+        playerCountry : "England",
+        playerImage : require ("./lingard.png"),
+        playerName : "Jesse Lingard"
+    },
+    {
+        playerId : 16,
+        playerPosition : "Midfielder",
+        playerCountry : "Scotland",
+        playerImage : require ("./mctominay.png"),
+        playerName : "Scott McTominay"
+    },
+    {
+        playerId : 17 ,
+        playerPosition : "Midfielder",
+        playerCountry : "Serbia",
+        playerImage : require ("./matic.png"),
+        playerName : "Nemanja Matic"
+    },
+    {
+        playerId : 18 ,
+        playerPosition : "Midfielder",
+        playerCountry : "Brazil",
+        playerImage : require ("./fred.png"),
+        playerName : "Fred"
+    },
+    {
+        playerId : 19 ,
+        playerPosition : "Midfielder",
+        playerCountry : "France",
+        playerImage : require ("./pogba.png"),
+        playerName : "Paul Pogba"
+    },
+    {
+        playerId : 20 ,
+        playerPosition : "Midfielder",
+        playerCountry : "Portugal",
+        playerImage : require ("./fernandes.png"),
+        playerName : "Bruno Fernandes"
+    },
+    {
+        playerId : 21 ,
+        playerPosition : "Attacker",
+        playerCountry : "Portugal",
+        playerImage : require ("./ronaldo.png"),
+        playerName : "Christiano Ronaldo"
+    },
+    {
+        playerId : 22 ,
+        playerPosition : "Attacker",
+        playerCountry : "England",
+        playerImage : require ("./rushford.png"),
+        playerName : "Marcus Rushford"
+    },
+    {
+        playerId : 23 ,
+        playerPosition : "Attacker",
+        playerCountry : "England",
+        playerImage : require ("./sancho.png"),
+        playerName : "Jadon Sancho"
+    },
+    {
+        playerId : 24 ,
+        playerPosition : "Attacker",
+        playerCountry : "Tunisia",
+        playerImage : require ("./mejbri.png"),
+        playerName : "Hannibal Mejbri"
+    },
+    {
+        playerId : 25 ,
+        playerPosition : "Attacker",
+        playerCountry : "Uruguay",
+        playerImage : require ("./cavani.png"),
+        playerName : "Edinson Cavani"
+    },
+    {
+        playerId : 26 ,
+        playerPosition : "Attacker",
+        playerCountry : "England",
+        playerImage : require ("./greenwood.png"),
+        playerName : "Mason Greenwood"
+    },
+    {
+        playerId : 27 ,
+        playerPosition : "Attacker",
+        playerCountry : "England",
+        playerImage : require ("./shoretire.png"),
+        playerName : "Shola Shoretire"
+    },
+    {
+        playerId : 28 ,
+        playerPosition : "Attacker",
+        playerCountry : "Sweden",
+        playerImage : require ("./elanga.png"),
+        playerName : "Anthony Elanga"
+    },
+
+
+
+]
+export default manUnitedPlayerData
