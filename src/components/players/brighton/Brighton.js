dcd8b91e7f3f621dfa864ac930797e14c26c654c
JeffVeron <67024868+JeffVeron@users.noreply.github.com>
2022-06-20 20:09:45 +0000

    Add files via upload

---
+++ src/components/players/brighton/Brighton.js
@@ -0,0 +1,216 @@
+const brightonPlayerData = [
+    {
+        playerId : 1 ,
+        playerName : "Jason Steele",
+        playerPosition : "Goalkeeper",
+        playerCountry : "England" ,
+        playerImage : require ("./steele.png")
+    },
+    {
+        playerId : 2 ,
+        playerName : "Robert Sánchez",
+        playerPosition : "Goalkeeper" ,
+        playerCountry : "Spain ",
+        playerImage : require ("./sanchez.png")
+    },
+    {
+        playerId : 3 , 
+        playerName : "Thomas McGill",
+        playerPosition : "Goalkeeper",
+        playerCountry : "England" ,
+        playerImage : require ("./mcgill.png")
+    }, 
+    {
+        playerId : 4 ,
+        playerPosition : "Defender",
+        playerCountry : "England",
+        playerImage : require("./roberts.png"),
+        playerName : "HAydon Roberts"
+    },
+    {
+        playerId : 5 ,
+        playerPosition : "Defender",
+        playerCountry : "England",
+        playerImage : require("./turns.png"),
+        playerName : "Ed Turns"
+    },
+    {
+        playerId : 6 ,
+        playerPosition : "Defender",
+        playerCountry : "England",
+        playerImage : require("./dunk.png"),
+        playerName : "Lewis Dunk"
+    },
+    {
+        playerId : 7 ,
+        playerPosition : "Defender",
+        playerCountry : "England",
+        playerImage : require("./tsoungui.png"),
+        playerName : "Antef Tsoungui"
+    },
+    {
+        playerId : 8 ,
+        playerPosition : "Defender",
+        playerCountry : "England",
+        playerImage : require("./offiah.png"),
+        playerName : "Odel Offiah"
+    },
+    {
+        playerId : 9 ,
+        playerPosition : "Defender",
+        playerCountry : "England",
+        playerImage : require("./webster.png"),
+        playerName : "Adam Webster"
+    },
+    {
+        playerId : 10 ,
+        playerPosition : "Defender",
+        playerCountry : "Netherlands",
+        playerImage : require("./veltman.png"),
+        playerName: "Joel Veltman"
+    },
+    {
+        playerId : 11 ,
+        playerPosition : "Defender",
+        playerCountry : "Ireland",
+        playerImage : require("./duffy.png"),
+        playerName : "Shane Duffy"
+    },
+    {
+        playerId : 12 ,
+        playerPosition : "Defender",
+        playerCountry : "Spain",
+        playerImage : require("./cucurella.png"),
+        playerName : "Marc Cucurella"
+    },
+    {
+        playerId : 13 ,
+        playerPosition : "Defender",
+        playerCountry : "Ireland",
+        playerImage : require("./furlong.png"),
+        playerName : "James Furlong"
+    },
+    {
+        playerId : 14 ,
+        playerPosition : "Defender",
+        playerCountry : "Ireland",
+        playerImage : require("./moran.png"),
+        playerName : "Andrew Moran"
+    },
+    {
+        playerId : 15 ,
+        playerPosition : "Midfielder",
+        playerCountry : "England",
+        playerImage : require("./lallana.png"),
+        playerName : "Adam Lallana"
+    },
+    {
+        playerId : 16 ,
+        playerPosition : "Midfielder",
+        playerCountry : "England",
+        playerImage : require("./sollyMarch.png"),
+        playerName : "Solly March"
+    },
+    {
+        playerId : 17 ,
+        playerPosition : "Midfielder",
+        playerCountry : "England",
+        playerImage : require("./lamptey.png"),
+        playerName : "Tariq Lamptey"
+    } ,
+    {
+        playerId : 18 ,
+        playerPosition : "Midfielder",
+        playerCountry : "Mali",
+        playerImage : require("./bissouma.png"),
+        playerName : "Yves Bissouma"
+    },
+    {
+        playerId : 19 ,
+        playerPosition : "Midfielder",
+        playerCountry : "Zambia",
+        playerImage : require("./mwepu.png"),
+        playerName : "Enock Mwepu"
+    },
+    {
+        playerId : 20 ,
+        playerPosition : "Midfielder",
+        playerCountry : "Scotland",
+        playerImage : require("./leonard.png"),
+        playerName : "Marc Leonard"
+    },
+    {
+        playerId : 21 ,
+        playerPosition : "Midfielder",
+        playerCountry : "Germany",
+        playerImage : require("./gross.png"),
+        playerName : "Pascal Gross"
+    },
+    {
+        playerId : 22 ,
+        playerPosition : "Midfielder",
+        playerCountry : "Colombia",
+        playerImage : require("./alzate.png"),
+        playerName : "Steven Alzate"
+    },
+    {
+        playerId : 23 ,
+        playerPosition : "Midfielder",
+        playerCountry : "Poland",
+        playerImage : require("./moder.png"),
+        playerName : "Jacob Moder "
+    },
+     {
+        playerId : 24 ,
+        playerPosition : "Midfielder",
+        playerCountry : "Ecuador",
+        playerImage : require("./caicedo.png"),
+        playerName : "Moisés Caicedo"
+     },
+     {
+        playerId : 25 ,
+        playerPosition : "Midfielder",
+        playerCountry : "Argentina",
+        playerImage : require("./macAllister.png"),
+        playerName : "Alexis Mac Allister"
+     },
+     {
+        playerId : 26 ,
+        playerPosition : "Midfielder",
+        playerCountry : "Ecuador",
+        playerImage : require("./sarmiento.png"),
+        playerName : "Jeremy Sarmiento"
+     },
+     {
+        playerId : 27 ,
+        playerPosition : "Attacker",
+        playerCountry : "England",
+        playerImage : require("./welbeck.png"),
+        playerName : "Danny welbeck "
+         
+     },
+     {
+        playerId : 28 ,
+        playerPosition : "Attacker",
+        playerCountry : "Ireland",
+        playerImage : require("./ferguson.png"),
+        playerName : "Evan Ferguson"
+     },
+     {
+        playerId : 29 ,
+        playerPosition : "Attacker",
+        playerCountry : "Belgium",
+        playerImage : require("./trossard.png"),
+        playerName : "Leandro Trossard"
+     },
+     {
+        playerId : 30 ,
+        playerPosition : "Attacker",
+        playerCountry : "France",
+        playerImage : require("./maupay.png"),
+        playerName : "Neal Maupay"
+     }
+
+
+]
+export default brightonPlayerData
