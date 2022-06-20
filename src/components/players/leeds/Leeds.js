7545917a26caad4012ff56254fcff61b0161caa6
JeffVeron <67024868+JeffVeron@users.noreply.github.com>
2022-06-20 20:16:33 +0000

    Add files via upload

---
+++ src/components/players/leeds/Leeds.js
@@ -0,0 +1,219 @@
+const leedsPlayerData = [
+    {
+        playerId : 1 ,
+        playerPosition : "Goalkeeper" ,
+        playerCountry : "Norway",
+        playerImage : require ("./klaesson.png"),
+        playerName : " Kristoffer Klaesson"
+
+    },
+    {
+        playerId : 2 ,
+        playerPosition : "Goalkeeper" ,
+        playerCountry : "France",
+        playerImage : require ("./meslier.png"),
+        playerName : "Illian Meslier "
+
+    },
+    {
+        playerId : 3 ,
+        playerPosition : "Defender" ,
+        playerCountry : "Scotland",
+        playerImage : require ("./cooper.png"),
+        playerName : " Liam Cooper"
+
+    },
+    {
+        playerId : 4 ,
+        playerPosition : "Defender" ,
+        playerCountry : "Spain",
+        playerImage : require ("./llorente.png"),
+        playerName : "Diego LLorente "
+
+    },
+    {
+        playerId : 5 ,
+        playerPosition : "Defender" ,
+        playerCountry : "England",
+        playerImage : require ("./shackleton.png"),
+        playerName : " Jamie Shackleton"
+
+    },
+    {
+        playerId : 6 ,
+        playerPosition : "Defender" ,
+        playerCountry : "England",
+        playerImage : require ("./moore.png"),
+        playerName : " Chris Moore "
+
+    },
+    {
+        playerId : 7 ,
+        playerPosition : "Defender" ,
+        playerCountry : "England",
+        playerImage : require ("./ayling.png"),
+        playerName : "Luke Ayling"
+
+    },
+    {
+        playerId : 8 ,
+        playerPosition : "Defender" ,
+        playerCountry : "Spain",
+        playerImage : require ("./firpo.png"),
+        playerName : " Junior FIrpo"
+
+    },
+    {
+        playerId : 9 ,
+        playerPosition : "Defender" ,
+        playerCountry : "England",
+        playerImage : require ("./greenwood.png"),
+        playerName : "Sam Greenwood "
+
+    },
+    {
+        playerId : 10 ,
+        playerPosition : "Defender" ,
+        playerCountry : "Norway",
+        playerImage : require ("./hjelde.png"),
+        playerName : " Leo Hjelde"
+
+    },
+    {
+        playerId :11 ,
+        playerPosition : "Defender" ,
+        playerCountry : "Northen Ireland",
+        playerImage : require ("./dallas.png"),
+        playerName : " Staurt Dallas"
+
+    },
+    {
+        playerId : 12 ,
+        playerPosition : "Defender" ,
+        playerCountry : "Netherlands",
+        playerImage : require ("./struijk.png"),
+        playerName : " Pascal Struijk"
+
+    },
+    {
+        playerId : 13 ,
+        playerPosition : "Defender" ,
+        playerCountry : "England",
+        playerImage : require ("./creswell.png"),
+        playerName : " Charlie Cresswell"
+
+    },
+    {
+        playerId : 14 ,
+        playerPosition : "Midfielder" ,
+        playerCountry : "Poland",
+        playerImage : require ("./klich.png"),
+        playerName : " Mateusz Klich"
+    },
+    {
+        playerId : 15 ,
+        playerPosition : "Midfielder" ,
+        playerCountry : "England",
+        playerImage : require ("./phillips.png"),
+        playerName : " Kalvin Phillips"
+    },
+    {
+        playerId : 16 ,
+        playerPosition : "Midfielder" ,
+        playerCountry : "Scotland",
+        playerImage : require ("./mckinstry.png"),
+        playerName : "Stuart McKinstry "
+    },
+    {
+        playerId : 17 ,
+        playerPosition : "Midfielder" ,
+        playerCountry : "England",
+        playerImage : require ("./kenneh.png"),
+        playerName : " Nohan Kenneh  "
+    },
+    {
+        playerId : 18 ,
+        playerPosition : "Midfielder" ,
+        playerCountry : "England",
+        playerImage : require ("./forshaw.png"),
+        playerName : " Adam Forshaw"
+    },
+    {
+        playerId : 19 ,
+        playerPosition : "Midfielder" ,
+        playerCountry : "Germany",
+        playerImage : require ("./koch.png"),
+        playerName : " Robin Koch"
+    },
+    {
+        playerId : 20 ,
+        playerPosition : "Midfielder" ,
+        playerCountry : "England",
+        playerImage : require ("./bate.png"),
+        playerName : " Lewis Bate"
+    },
+    {
+        playerId : 21 ,
+        playerPosition : "Midfielder" ,
+        playerCountry : "Spain",
+        playerImage : require ("./rodrigo.png"),
+        playerName : "Rodrigo "
+    },
+    {
+        playerId : 22 ,
+        playerPosition : "Midfielder" ,
+        playerCountry : "Wales",
+        playerImage : require ("./roberts.png"),
+        playerName : "Tyler Roberts "
+    },
+    {
+        playerId : 23 ,
+        playerPosition : "Midfielder" ,
+        playerCountry : "England",
+        playerImage : require ("./jenkins.png"),
+        playerName : "Jack Jenkins "
+    },
+    {
+        playerId : 24 ,
+        playerPosition : "Attacker" ,
+        playerCountry : "England",
+        playerImage : require ("./bamford.png"),
+        playerName : "Patrick Bamford "
+    },
+    {
+        playerId : 25 ,
+        playerPosition : "Attacker" ,
+        playerCountry : "England",
+        playerImage : require ("./harrison.png"),
+        playerName : " Jack Harrison"
+    },
+    {
+        playerId : 26 ,
+        playerPosition : "Attacker" ,
+        playerCountry : "England",
+        playerImage : require ("./gray.png"),
+        playerName : "Archie Gray "
+    },
+    {
+        playerId : 27 ,
+        playerPosition : "Attacker" ,
+        playerCountry : "England",
+        playerImage : require ("./gelhardt.png"),
+        playerName : "Joe Gelhart "
+    },
+    {
+        playerId : 28 ,
+        playerPosition : "Attacker" ,
+        playerCountry : "Brazil",
+        playerImage : require ("./raphinha.png"),
+        playerName : " Raphinha"
+    },
+    {
+        playerId : 29 ,
+        playerPosition : "Attacker" ,
+        playerCountry : "Netherlands",
+        playerImage : require ("./summerville.png"),
+        playerName : "Crysencio Summerville "
+    },
+]
+export default  leedsPlayerData
---
+++ src/components/players/leicester/Leicester.js
@@ -0,0 +1,200 @@
+const leicesterPlayerData = [
+    {
+        playerId : 1 ,
+        playerPosition : " Goalkeeper",
+        playerCountry : "Denmark",
+        playerImage : require("./schmeichel.png"),
+        playerName : "Kasper Schmeichel"
+    },
+    {
+        playerId : 2,
+        playerPosition : " Goalkeeper",
+        playerCountry : "Switzerland",
+        playerImage : require("./jakupovic.png"),
+        playerName : "Eldin Jakupovic"
+    },
+
+    {
+        playerId : 3 ,
+        playerPosition : " Goalkeeper",
+        playerCountry : "Wales",
+        playerImage : require("./ward.png"),
+        playerName : "Danny Ward"
+    },
+    {
+        playerId : 4 ,
+        playerPosition : " Defender",
+        playerCountry : "Northen Ireland",
+        playerImage : require("./evans.png"),
+        playerName : "Jonny Evans"
+    },
+    {
+        playerId : 5 ,
+        playerPosition : " Defender",
+        playerCountry : "Portugal",
+        playerImage : require("./pereira.png"),
+        playerName : "Ricardo Pereira"
+    },
+    {
+        playerId : 6 ,
+        playerPosition : " Defender",
+        playerCountry : "France",
+        playerImage : require("./fofana.png"),
+        playerName : "Wesley Forfana"
+    },
+    {
+        playerId : 7 ,
+        playerPosition : " Defender",
+        playerCountry : "Turkey",
+        playerImage : require("./soyuncu.png"),
+        playerName : "Caglar Söyüncü"
+    },
+    {
+        playerId : 8 ,
+        playerPosition : " Defender",
+        playerCountry : "England",
+        playerImage : require("./bertrand.png"),
+        playerName : "Ryan Bertrand"
+    },
+    {
+        playerId : 9 ,
+        playerPosition : " Defender",
+        playerCountry : "Ghana",
+        playerImage : require("./amartey.png"),
+        playerName : "Daniel Amartey"
+    },
+    {
+        playerId : 10 ,
+        playerPosition : " Defender",
+        playerCountry : "England",
+        playerImage : require("./justin.png"),
+        playerName : "James Justin "
+    },
+    {
+        playerId : 11 ,
+        playerPosition : " Defender",
+        playerCountry : "Denmark",
+        playerImage : require("./vestergaard.png"),
+        playerName : "Jannick Vestergaard"
+    },
+    {
+        playerId : 12 ,
+        playerPosition : " Defender",
+        playerCountry : "Belgium",
+        playerImage : require("./castagne.png"),
+        playerName : "Timothy Castagne"
+    },
+    {
+        playerId : 13 ,
+        playerPosition : " Defender",
+        playerCountry : "England",
+        playerImage : require("./thomas.png"),
+        playerName : "Luke Thomas"
+    },
+    {
+        playerId : 14 ,
+        playerPosition : " Midfielder",
+        playerCountry : "Senegal",
+        playerImage : require("./mendy.png"),
+        playerName : "Nampalys Mendy"
+    },
+    {
+        playerId : 15 ,
+        playerPosition : " Midfielder",
+        playerCountry : "England",
+        playerImage : require("./maddison.png"),
+        playerName : "James Maddison"
+    },
+    {
+        playerId : 16 ,
+        playerPosition : " Midfielder",
+        playerCountry : "France",
+        playerImage : require("./soumare.png"),
+        playerName : "Boubakary Soumare"
+    },
+    {
+        playerId : 17 ,
+        playerPosition : " Midfielder",
+        playerCountry : "Belgium",
+        playerImage : require("./tielemans.png"),
+        playerName : "Youri Tielemans"
+    },
+    {
+        playerId : 18 ,
+        playerPosition : " Midfielder",
+        playerCountry : "Nigeria",
+        playerImage : require("./ndidi.png"),
+        playerName : "Wilfred Ndidi"
+    },
+    {
+        playerId : 19 ,
+        playerPosition : " Midfielder",
+        playerCountry : "England",
+        playerImage : require("./dewsbury-hall.png"),
+        playerName : "Kiernan Dewsbury-Hall"
+    },
+    {
+        playerId : 20 ,
+        playerPosition : " Midfielder",
+        playerCountry : "Spain",
+        playerImage : require("./perez.png"),
+        playerName : "Ayoze Perez"
+    },
+    {
+        playerId : 21 ,
+        playerPosition : " Midfielder",
+        playerCountry : "England",
+        playerImage : require("./choudhury.png"),
+        playerName : "Hamza Choudry"
+    },
+    {
+        playerId : 22 ,
+        playerPosition : " Attacker",
+        playerCountry : "England",
+        playerImage : require("./albrighton.png"),
+        playerName : "Marc Albrighton"
+    },
+    {
+        playerId : 23 ,
+        playerPosition : " Attacker",
+        playerCountry : "Zambia",
+        playerImage : require("./daka.png"),
+        playerName : "Patson Daka"
+    },
+    {
+        playerId : 24 ,
+        playerPosition : " Attacker",
+        playerCountry : "England",
+        playerImage : require("./vardy.png"),
+        playerName : "Jamie Vardy"
+    },
+    {
+        playerId : 25 ,
+        playerPosition : " Attacker",
+        playerCountry : "Nigeria",
+        playerImage : require("./lookman.png"),
+        playerName : "Ademola Lookman"
+    },
+    {
+        playerId : 26 ,
+        playerPosition : " Attacker",
+        playerCountry : "Nigeria",
+        playerImage : require("./iheanacho.png"),
+        playerName : "Kelechi Iheanacho"
+    },
+    {
+        playerId : 27 ,
+        playerPosition : " Attacker",
+        playerCountry : "England",
+        playerImage : require("./barnes.png"),
+        playerName : "Harvey Barnes"
+    },
+
+
+
+
+
+
+
+]
+export default leicesterPlayerData
